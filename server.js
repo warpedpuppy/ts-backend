1ad7f1d1e12a44b942636f7ddf87cad2a44be1f3
ted <ted@warpedpuppy.com>
2021-02-09 11:58:36 -0500

    graphql change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const app = express();
-const TestModel = require('./models/testModel')
+// const TestModel = require('./models/testModel')
 const Config = require('./config');
 const cors = require('cors');
 
@@ -14,13 +14,13 @@ app.use(cors);
 mongoose.connect(Config.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true })
 .catch(error => console.error(error));
 
-TestModel.find()
-.then(result => console.log(result))
+// TestModel.find()
+// .then(result => console.log(result))
 
-app.use('/create', (req, res) => {
-    TestModel.create({name: "test", size: "test"})
-.then(result => console.log(result))
-})
+// app.use('/create', (req, res) => {
+//     TestModel.create({name: "test", size: "test"})
+// .then(result => console.log(result))
+// })
 
 
 app.listen(Config.PORT, () => console.log(`listening to ${Config.PORT}`))
