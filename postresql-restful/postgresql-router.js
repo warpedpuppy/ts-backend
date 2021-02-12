1cf79e66f82c3cccf848400b4b9d418ae417e4e6
ted <ted@warpedpuppy.com>
2021-02-12 13:54:08 -0500

    complete postres restful routes

--- postresql-restful/postgresql-router.js
+++ postresql-restful/postgresql-router.js
@@ -12,13 +12,37 @@ PostgresQLRouter
    
 })
 .post('/', async (req, res) => {
+    const { character_name, character_color } = req.body;
     try {
-        let result = await PostgresQLServices.insertOne(req.app.get('db'), {character_name: "testName", character_color: "#FFOOFF"})
-        res.status(200).json({success: true})
+        let characters = await PostgresQLServices.insertOne(req.app.get('db'), { character_name, character_color })
+        res.status(200).json({success: true, characters})
     } catch (e) {
         console.error(e)
     }
 
 })
-
+.put('/', async (req, res) => {
+    try {
+        let characters = await PostgresQLServices.updateOne(req.app.get('db'), req.body)
+        res.status(200).json({success: true, characters})
+    } catch (e) {
+        console.error(e)
+    }
+})
+.delete('/', async (req, res) => {
+    try {
+        let characters = await PostgresQLServices.deleteOne(req.app.get('db'), req.body.id)
+        res.status(200).json({success: true, characters})
+    } catch (e) {
+        console.error(e)
+    }
+})
+.delete('/delete-all', async (req, res) => {
+    try {
+        let characters = await PostgresQLServices.deleteAll(req.app.get('db'))
+        res.status(200).json({success: true, characters})
+    } catch (e) {
+        console.error(e)
+    }
+})
 module.exports = PostgresQLRouter;
--- postresql-restful/postgresql-services.js
+++ postresql-restful/postgresql-services.js
@@ -1,26 +1,46 @@
 const PostgresQLRouter = require("./postgresql-router")
-
 const xss = require('xss')
 const PostgresQLServices = {
     getAll: async db => {
         try {
-            let result = await db.select('*').from('characters');
-            console.log(result)
-            return result;
+            let characters = await db.select('*').from('characters');
+            return characters.map( character => serialize(character) )
         } catch (e) {
             return "error"
         }
-       
     },
-    insertOne: function (db, obj) {
+    insertOne: async (db, obj) => {
         try {
-            return db
-            .insert(obj)
-            .into('characters')
+            let result = await db.insert(obj).into('characters');
+            let characters = result ? await db.select('*').from('characters') : [] ;
+            return characters.map( character => serialize(character) )
         } catch (e) {
             console.error(e)
         }
-      
+    },
+    deleteOne: async (db, id) => {
+        let int = parseInt(id, 10)
+        let result = await db.raw(`DELETE FROM characters WHERE id=${int}`);
+        let characters = result ? await db.select('*').from('characters') : [] ;
+        return characters;
+    },
+    updateOne: async (db, obj) => {
+        const { id, character_name, character_color } = obj;
+        let result = await db.raw(`UPDATE characters SET character_name='${character_name}', character_color='${character_color}' WHERE id=${id}`)
+        let characters = result ? await db.select('*').from('characters') : [] ;
+        return characters;
+    },
+    deleteAll: async db => {
+        let result = await db.truncate('characters');
+        let characters = result ? await db.select('*').from('characters') : [] ;
+        return characters;
+    }
+}
+function serialize(obj) {
+    return {
+        id: obj.id,
+        character_name: xss(obj.character_name),
+        character_color: xss(obj.character_color)
     }
 }
 module.exports = PostgresQLServices;
