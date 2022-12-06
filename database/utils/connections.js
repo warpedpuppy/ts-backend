46ae8c639f3c1752289a884b66f9d20d1edc8256
ted <ted@warpedpuppy.com>
2022-12-06 13:44:07 -0500

    added package

--- database/utils/connections.js
+++ database/utils/connections.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const knex = require('knex')
+const knex = require('knex');
+const knexServerlessMysql = require('knex-serverless-mysql');
 // const Config = require('../../config');
 // const CharacterModel = require('../models/characterModel');
 // const uuid = require('uuid');
