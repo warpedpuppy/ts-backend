350deb341fe88d3cd3c07b5e94b08d7d40dfe07d
ted <ted@warpedpuppy.com>
2022-12-06 13:29:09 -0500

    added package

--- database/utils/connections.js
+++ database/utils/connections.js
@@ -37,27 +37,27 @@ async function startMongo () {
 
 //POSTGRESQL
 function startPostgres (app) {
-//   try {
-//     let postgres_connection = process.env.ENVELOPE === 'local' ? process.env.POSTGRES_LOCAL : process.env.POSTGRES_REMOTE;
-//     const knex = require('knex')
-//     const db = knex({
-//       client: 'pg',
-//       connection: postgres_connection,
-//     })
+  try {
+    let postgres_connection = process.env.ENVELOPE === 'local' ? process.env.POSTGRES_LOCAL : process.env.POSTGRES_REMOTE;
+    const knex = require('knex')
+    // const db = knex({
+    //   client: 'pg',
+    //   connection: postgres_connection,
+    // })
 
-//     if (process.env.ENVELOPE === 'local') {
-//       console.log(`connected to local posgresql db`)
-//     } else {
-//       console.log(`connected to remote posgresql db`)
-//     }
-//     app.set('db', db)
-//   } catch (e) {
-//       if (process.env.ENVELOPE === 'local') {
-//         console.log(`problem connecting to local posgresql db`)
-//       } else {
-//         console.log(`problem connecting to remote posgresql db`)
-//       }
-//   }
+    if (process.env.ENVELOPE === 'local') {
+      console.log(`connected to local posgresql db`)
+    } else {
+      console.log(`connected to remote posgresql db`)
+    }
+    app.set('db', db)
+  } catch (e) {
+      if (process.env.ENVELOPE === 'local') {
+        console.log(`problem connecting to local posgresql db`)
+      } else {
+        console.log(`problem connecting to remote posgresql db`)
+      }
+  }
 }
 // startGraphQL
 module.exports = { startMongo, startPostgres  }
